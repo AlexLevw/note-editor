d144828d7aece82c1aa44836ec0bdafef45eb20b
Alex <foxi4554@gmail.com>
2021-06-11 16:06:05 +0300

    feat: css preprocessor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
+      Page
     </div>
   );
 }
